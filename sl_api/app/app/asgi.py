2a8f4340dc5d01009e2c63a3aa53847799a00a4a
bpedrazaa <bpedrazaa@gmail.com>
2024-07-01 16:18:49 -0400

    Create base API and UI

---
+++ sl_api/app/app/asgi.py
@@ -0,0 +1,14 @@
+﻿"""
+ASGI config for app project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+from django.core.asgi import get_asgi_application
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+application = get_asgi_application()
---
+++ sl_api/app/core/migrations/0002_create_groups.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import Group, Permission
+from django.db import migrations
+
+def create_groups(apps, schema_editor):
+    instructors, _ = Group.objects.get_or_create(name='instructors')
+    students, _ = Group.objects.get_or_create(name='students')
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('core', '0001_initial')
+    ]
+
+    operations = [
+        migrations.RunPython(create_groups),
+    ]
