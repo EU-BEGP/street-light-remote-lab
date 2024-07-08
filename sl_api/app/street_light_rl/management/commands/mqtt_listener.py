8a9b77a1bab31ebd4cb36f8c32bbaf5b80f9d2e7
bpedrazaa <bpedrazaa@gmail.com>
2024-07-08 16:22:41 -0400

    Implement mqtt listener command in django application

---
+++ sl_api/app/street_light_rl/management/commands/mqtt_listener.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+import paho.mqtt.client as mqtt
+
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
+import os
+import json
+
+mqtt_host = os.environ.get("MQTT_HOST")
+mqtt_port = os.environ.get("MQTT_PORT")
+mqtt_topic = os.environ.get("MQTT_TOPIC")
+
+channel_layer = get_channel_layer()
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT broker")
+        client.subscribe(mqtt_topic)
+    else:
+        print("Failed to connect to MQTT broker. Error code:", rc)
+
+
+def on_message(client, userdata, msg):
+    message_content = msg.payload.decode()
+    message_object = json.loads(message_content)
+    print(message_object)
+
+    # Send data to WebSocket consumers
+    async_to_sync(channel_layer.group_send)(
+        "data_group",  # Group name to send to
+        {"type": "send_websocket_data", "data": json.dumps(message_object)},
+    )
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        client = mqtt.Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
+
+        client.connect(mqtt_host, int(mqtt_port), 60)
+        client.loop_forever()
