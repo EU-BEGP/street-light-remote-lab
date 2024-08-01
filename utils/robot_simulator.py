49df549adbb9ca9e25185b612a28a88459474256
bpedrazaa <bpedrazaa@gmail.com>
2024-08-01 11:06:24 -0400

    Format robot simulator file and create new directory to store it

--- utils/robot_simulator.py
+++ utils/robot_simulator.py
@@ -1,49 +1,56 @@
-#File: robotSimulator.py
-#Purpose: A robot data simulator for UPB remote lab.
-#Author: Maria Fernanda del Granado
+# File: robot_simulator.py
+# Purpose: A robot data simulator for UPB remote lab.
+# Author: Maria Fernanda del Granado
 
 import random
 import math
 import json
-import time 
+import time
 import sys
 import paho.mqtt.client as mqtt
 
+# Define mqtt constants
+MQTT_PORT = None
+MQTT_HOST = None
+MQTT_TOPIC = None
 
 """
 Class that allows user to create robot object given the following:
--A robot ID 
--The width of the grid 
+-A robot ID
+-The width of the grid
 -The height of the grid
 -The upper intensity value
 -The lower intensity value
 """
-class Robot():
-    def __init__ (self, ID, width, height, lower_intensity_val, upper_intensity_val):
+
+
+class Robot:
+    def __init__(self, ID, width, height, lower_intensity_val, upper_intensity_val):
         self.width = width
         self.height = height
         self.ID = ID
         self.upper_intensity_val = upper_intensity_val
         self.lower_intensity_val = lower_intensity_val
-        self.results = [] # 2D list
-        self.final_result_strings = [] 
-    
+        self.results = []  # 2D list
+        self.final_result_strings = []
+
     """
     Function generates data based on distance from center and intensity of distance between grid squares and center.
     """
+
     def generate_data(self):
         center_x = self.width // 2
         center_y = self.height // 2
 
-        # Distance from the center to farthest corner 
-        max_distance = math.sqrt(center_x ** 2 + center_y ** 2) 
+        # Distance from the center to farthest corner
+        max_distance = math.sqrt(center_x**2 + center_y**2)
 
         for i in range(self.height):
             temp_list = []
 
             for j in range(self.width):
                 # Calculating distance between squares in the grid
-                distance = math.sqrt((i - center_y) ** 2 + (j - center_x) ** 2) 
+                distance = math.sqrt((i - center_y) ** 2 + (j - center_x) ** 2)
                 intensity = self.calculate_intensity(distance, max_distance)
                 temp_list.append(intensity)
 
@@ -53,15 +60,21 @@ class Robot():
     """
     Function calculates intensity of the distance.
     """
+
     def calculate_intensity(self, distance, max_distance):
         # Highest value times the proportion times the range
-        return int(self.upper_intensity_val - (distance / max_distance) * (self.upper_intensity_val - self.lower_intensity_val))
+        return int(
+            self.upper_intensity_val
+            - (distance / max_distance)
+            * (self.upper_intensity_val - self.lower_intensity_val)
+        )
 
     """
     Helper function to print out results as grid.
     """
+
     def print_grid(self):
-        grid_tops = "+------"  * self.width + "+"
+        grid_tops = "+------" * self.width + "+"
         sides = "|      " * self.width + "|"
 
         for i in range(self.height):
@@ -69,28 +82,28 @@ class Robot():
             temp_string = ""
 
             for j in range(self.width):
-
                 if j == 0:
                     if self.results[i][j] > 9:
                         temp_string += f"|  {self.results[i][j]}  |"
 
-                    else: 
+                    else:
                         temp_string += f"|  {self.results[i][j]}   |"
 
                 else:
                     if self.results[i][j] > 9:
                         temp_string += f"  {self.results[i][j]}  |"
-                    else: 
+                    else:
                         temp_string += f"   {self.results[i][j]}  |"
             print(sides)
             print(temp_string)
             print(sides)
 
         print(grid_tops)
-                
+
     """
     Function formats strings results.
     """
+
     def create_result_strings_format(self):
         for i in range(self.height):
             for j in range(self.width):
@@ -101,6 +114,7 @@ class Robot():
     """
     Prints results.
     """
+
     def print_results(self):
         print("\nData: ")
 
@@ -109,28 +123,31 @@ class Robot():
         print("")
 
     """
-    Function that connects client to mqtt host and port, then publishes data in payloads. 
+    Function that connects client to mqtt host and port, then publishes data in payloads.
     """
+
     def send_json_data(self):
-        client = mqtt.Client(client_id = "", clean_session = True, userdata = None, protocol = mqtt.MQTTv311)
+        client = mqtt.Client(
+            client_id="", clean_session=True, userdata=None, protocol=mqtt.MQTTv311
+        )
 
         # Handles connection error case
-        if client.connect("eubbc-digital.upb.edu", 1884, 40) != 0:
+        if client.connect(MQTT_HOST, MQTT_PORT, 40) != 0:
             print("Could not connect!")
             sys.exit(-1)
 
         for i in range(self.height):
             for j in range(self.width):
-                if (i == self.height -1) and (j == self.width - 1):
+                if (i == self.height - 1) and (j == self.width - 1):
                     continue
                 data = {
-                    "ID": self.ID, # Only strings should be in quotes when sending in JSON format
+                    "ID": self.ID,  # Only strings should be in quotes when sending in JSON format
                     "x_pos": i,
                     "y_pos": j,
                     "intensity": self.results[i][j],
-                    "finish": False # Send all messages with "finish" set to false except for last
+                    "finish": False,  # Send all messages with "finish" set to false except for last
                 }
-                client.publish("street-light-rl/data", json.dumps(data)) # Publish to topic
+                client.publish(MQTT_TOPIC, json.dumps(data))  # Publish to topic
 
         # Send the last message with "finish" set to true
         last_data = {
@@ -138,20 +155,19 @@ class Robot():
             "x_pos": self.height - 1,
             "y_pos": self.width - 1,
             "intensity": self.results[-1][-1],
-            "finish": True
+            "finish": True,
         }
-        client.publish("street-light-rl/data", json.dumps(last_data))
+        client.publish(MQTT_TOPIC, json.dumps(last_data))
         client.disconnect()
 
 
 if __name__ == "__main__":
-
-    while True: 
-        id_num= random.randrange(1, 5)
+    while True:
+        id_num = random.randrange(1, 5)
         curr_id = "robot" + str(id_num)
         robot = Robot(curr_id, 5, 5, 5, 20)
         robot.generate_data()
         robot.print_results()
         robot.print_grid()
         robot.send_json_data()
-        time.sleep(20) # 20 second time delay before next data is generated
+        time.sleep(20)  # 20 second time delay before next data is generated
