7bd6891b735ce7441df71b302b02a246316b78f7
ferdelgranado <delgranadom@carleton.edu>
2024-08-13 11:36:07 -0400

    Return user requested grid from query params (grid number, start and end date).

--- sl_api/app/street_light_rl/views.py
+++ sl_api/app/street_light_rl/views.py
@@ -2,25 +2,27 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from datetime import datetime, timedelta
 from .models import Robot, Experiment, Grid
 from .serializers import (
     RobotSerializer,
     ExperimentSerializer,
     GridSerializer,
 )
+import json
 
 
 class RobotListView(generics.ListAPIView):
     serializer_class = RobotSerializer
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = Robot.objects.all()
 
 
 class RobotExperimentsView(generics.ListAPIView):
     serializer_class = ExperimentSerializer
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         robot_id = self.kwargs["robot_id"]
@@ -30,15 +32,15 @@ class RobotExperimentsView(generics.ListAPIView):
 
 class ExperimentListView(generics.ListAPIView):
     serializer_class = ExperimentSerializer
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = Experiment.objects.all()
 
 
 class RobotGridsView(generics.ListAPIView):
     serializer_class = GridSerializer
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         robot_id = self.kwargs["robot_id"]
@@ -47,21 +49,34 @@ class RobotGridsView(generics.ListAPIView):
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset().order_by("id")
-
         grid_param = request.query_params.get("grid", None)
+        start_date_param = request.query_params.get("start", None)
+        end_date_param = request.query_params.get("end", None)
 
-        if grid_param:
+         # Returns an error message if the robot ID is invalid or if the robot has no associated grids.
+        if len(queryset) == 0:
+            return Response("Error: The provided Robot ID is invalid or the robot does not have any associated grids.", status=404)
+
+        # If only the grid query param is provided, the data is passed to a specific function for handling, which returns the corresponding grid.
+        if grid_param: 
             serializer_data = _handle_grid_param(self, grid_param, queryset)
+
+        # If only the start date or both start and end dates are provided as query parameters, 
+        # the data is passed to a specific function for processing, which returns the corresponding grids.
+        elif start_date_param and (end_date_param is None) and (grid_param is None):
+            serializer_data = _handle_date_params(self, start_date_param, end_date_param, queryset)
+        elif start_date_param and end_date_param and (grid_param is None):
+            serializer_data = _handle_date_params(self, start_date_param, end_date_param, queryset)
         else:
-            serializer_data = self.get_serializer(queryset, many=True).data
+            # If no query parameters are provided, or if incorrect parameters are used, return all grids associated with the specified robot ID.
+            serializer_data = self.get_serializer(queryset, many=True).data 
 
-        return Response(serializer_data)
-
+        return Response(serializer_data) # Returns the grid(s) that match the specified criteria.
 
 class ExperimentGridsView(generics.ListAPIView):
     serializer_class = GridSerializer
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,) 
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         experiment_id = self.kwargs["id"]
@@ -72,16 +87,29 @@ class ExperimentGridsView(generics.ListAPIView):
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset().order_by("id")
-
         grid_param = request.query_params.get("grid", None)
+        start_date_param = request.query_params.get("start", None)
+        end_date_param = request.query_params.get("end", None)
 
-        if grid_param:
+         # Returns an error message if the experiment ID is invalid or if the experiment has no associated grids.
+        if len(queryset) == 0:
+            return Response("Error: The provided Robot ID is invalid or the robot does not have any associated grids.", status=404)
+
+        # If only the grid query param is provided, the data is passed to a specific function for handling, which returns the corresponding grid.
+        if grid_param: 
             serializer_data = _handle_grid_param(self, grid_param, queryset)
+
+        # If only the start date or both start and end dates are provided as query parameters, 
+        # the data is passed to a specific function for processing, which returns the corresponding grids.
+        elif start_date_param and (end_date_param is None) and (grid_param is None):
+            serializer_data = _handle_date_params(self, start_date_param, end_date_param, queryset)
+        elif start_date_param and end_date_param and (grid_param is None):
+            serializer_data = _handle_date_params(self, start_date_param, end_date_param, queryset)
         else:
-            serializer_data = self.get_serializer(queryset, many=True).data
+            # If no query parameters are provided, or if incorrect parameters are used, return all grids associated with the specified experiment ID.
+            serializer_data = self.get_serializer(queryset, many=True).data 
 
-        return Response(serializer_data)
-
+        return Response(serializer_data) # Returns the grid(s) that match the specified criteria.
 
 def _handle_grid_param(self, grid_param, queryset):
     grid = None
@@ -89,7 +117,50 @@ def _handle_grid_param(self, grid_param, queryset):
         grid = queryset.last()
     elif grid_param == "first":
         grid = queryset.first()
+    else:
+        try:
+            # Converts 'grid_param' to an int and then retrieves the corresponding data from the queryset.
+            grid_param = int(grid_param)
+            grid = queryset[grid_param-1] 
+
+        except:
+            response = json.loads('{"Error": "Invalid grid number. Please enter a valid grid number within the range of available grids."}')
+            return response
 
     if grid is not None:
-        serializer = self.get_serializer(grid)
-        return serializer.data
+        serializer = self.get_serializer(grid) 
+        return serializer.data 
+        
+def _handle_date_params(self, start_date_param, end_date_param, queryset):
+    try:
+        date_format = '%Y-%m-%d' 
+        if '-' in start_date_param and (end_date_param is not None):
+            start_date_obj = (datetime.strptime(start_date_param, date_format)).date() 
+            # Add one day to the end date to include grids created at the end date (since the range ends at 00:00).
+            end_date_obj = (datetime.strptime(end_date_param, date_format)+ timedelta(days=1)).date()
+        elif '-' in start_date_param and (end_date_param is None):   
+            start_date_obj = (datetime.strptime(start_date_param, date_format)).date()
+
+        if start_date_obj and (end_date_param is None): 
+            current_date_obj = (datetime.now() + timedelta(days=1)).date()
+            # Returns an error message if the start date is later than today's date.
+            if start_date_obj > (current_date_obj - timedelta(days=1)): 
+                response = json.loads('{"Error": "The start date cannot be later than todays date."}')
+                return response
+            filtered_queryset = queryset.filter(timestamp__range=[start_date_obj, current_date_obj])
+
+        elif start_date_obj and end_date_obj:
+            # Returns an error message if the start date is after the end date. 
+            if start_date_obj > (end_date_obj - timedelta(days=1)):
+                response = json.loads('{"Error": "The start date cannot be after the end date."}')
+                return response
+            # Filters the queryset to include grids with dates within the specified start and end date range.
+            filtered_queryset = queryset.filter(timestamp__range=[start_date_obj, end_date_obj]) 
+
+    except: # Returns an error message if the date is in an incorrect format or if random strings/numbers are provided.
+        response = json.loads('{"Error": "Dates must be formatted as YYYY-MM-DD."}') 
+        return response
+
+    serializer_data = self.get_serializer(filtered_queryset, many=True).data 
+    return serializer_data # Returns the grid(s) that match the specified criteria.
+        
