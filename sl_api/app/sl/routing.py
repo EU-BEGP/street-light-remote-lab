2567df346bfbda4462539f7dc694be55f0f5bc84
bpedrazaa <bpedrazaa@gmail.com>
2025-02-07 11:11:28 -0400

    Add new routing setup based on the application environment

--- sl_api/app/sl/routing.py
+++ sl_api/app/sl/routing.py
@@ -1,13 +1,18 @@
 from django.urls import re_path
 from . import consumers
+import os
+
+env = os.environ.get("ENVIRONMENT")
+
+robot_path = (
+    r"sl/api/robot-information/$" if env == "production" else r"sl/robot-information/$"
+)
+
+light_path = (
+    r"sl/api/light-information/$" if env == "production" else r"sl/light-information/$"
+)
 
 websocket_urlpatterns = [
-    re_path(
-        r"sl/robot-information/$",
-        consumers.RobotInformationConsumer.as_asgi(),
-    ),
-    re_path(
-        r"sl/light-information/$",
-        consumers.LightInformationConsumer.as_asgi(),
-    ),
+    re_path(robot_path, consumers.RobotInformationConsumer.as_asgi()),
+    re_path(light_path, consumers.LightInformationConsumer.as_asgi()),
 ]
