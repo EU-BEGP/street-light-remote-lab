2d8eaa1a370faeb66ccbbb3a0db025ae1f2c7845
SebastianItamari <sebaton66@gmail.com>
2025-06-27 08:13:49 -0400

    (BACKEND): Update production values for robot_path and light_path in routing.py

--- sl_api/app/sl/routing.py
+++ sl_api/app/sl/routing.py
@@ -9,11 +9,11 @@ import os
 env = os.environ.get("ENVIRONMENT")
 
 robot_path = (
-    r"sl/api/robot-information/$" if env == "production" else r"sl/robot-information/$"
+    r"sl/api/ws/robot-information/$" if env == "production" else r"sl/robot-information/$"
 )
 
 light_path = (
-    r"sl/api/light-information/$" if env == "production" else r"sl/light-information/$"
+    r"sl/api/ws/light-information/$" if env == "production" else r"sl/light-information/$"
 )
 
 websocket_urlpatterns = [
