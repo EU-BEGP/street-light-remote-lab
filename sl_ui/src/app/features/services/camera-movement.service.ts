74a011f1e87299b2cefdd8d2378c68f36cde3e96
Sebastian Itamari <sebaton66@gmail.com>
2025-07-08 09:14:37 -0400

    (FRONTEND): Use HttpBackend in the camera-movement service to avoid the global loader interceptor

--- sl_ui/src/app/features/services/camera-movement.service.ts
+++ sl_ui/src/app/features/services/camera-movement.service.ts
@@ -3,7 +3,7 @@
 // Boris Pedraza, Alex Villazon, Omar Ormachea
 
 import config from 'src/app/config.json'
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpBackend } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -11,29 +11,31 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CameraMovementService {
-   private httpOptions = {
+  private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })
   };
   private URL: string = '';
+  private httpNoInterceptors: HttpClient;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private httpBackend: HttpBackend) {
     this.URL = `${config.api.baseUrl}`;
     this.httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
       }),
     };
+    this.httpNoInterceptors = new HttpClient(httpBackend);
   }
 
   moveCamera(direction: string, camera_number: number): Observable<any> {
     const body = { direction, camera_number };
-    return this.http.post(`${this.URL}${config.api.streetLights.cameraMovement.move}`, body, this.httpOptions);
+    return this.httpNoInterceptors.post(`${this.URL}${config.api.streetLights.cameraMovement.move}`, body, this.httpOptions);
   }
 
   stopCamera(camera_number: number): Observable<any> {
     const body = { camera_number };
-    return this.http.post(`${this.URL}${config.api.streetLights.cameraMovement.stop}`, body, this.httpOptions);
+    return this.httpNoInterceptors.post(`${this.URL}${config.api.streetLights.cameraMovement.stop}`, body, this.httpOptions);
   }
 }
